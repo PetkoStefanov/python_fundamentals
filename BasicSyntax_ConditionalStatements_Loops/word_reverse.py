4f2676dce197e9b075b15f6845573975e0caeae5
Petko Stefanov <122061006+PetkoStefanov@users.noreply.github.com>
2023-01-08 00:14:23 +0000

    Add files via upload

---
+++ BasicSyntax_ConditionalStatements_Loops/word_reverse.py
@@ -0,0 +1,10 @@
+word = str(input())
+word_reverse = word[::-1]
+print(word_reverse)
+
+# word=str(input())
+# reversed_word = ""
+# for i in range(len(word) - 1, -1, -1):
+#    reversed_word += word[i]
+
+# print(reversed_word)
